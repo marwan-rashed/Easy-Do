1579677fc89a85f8c2147f4e1f6196a82726a7e5
Marwan Rashed <marwanrashed300@gmail.com>
2022-03-13 23:07:43 +0200

    Add tasks controller

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -1,14 +1,153 @@
+import Task from "../models/task.js";
+import { v4 as uuid } from 'uuid';
+
 export default class TasksController {
-    static createTask(req, res) {}
+    static createTask(req, res) {
+        const { title, details, dueDate, listId } = req.body;
+        const taskId = uuid();
 
-    static getListTasks(req, res) {}
+        Task
+            .create({
+                taskId,
+                title,
+                details: details ? details : '',
+                dueDate: dueDate ? dueDate : 0,
+                completed: false,
+                listId,
+            })
+            .then(async() => {
+                let tasks = await Task.find({ listId }).select('-_id -__v');
 
-    static getTask(req, res) {}
+                res.status(200).json({
+                    success: true,
+                    data: {
+                        tasks,
+                    },
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json({
+                    success: false,
+                    message: 'Failed',
+                });
+            });
+    }
 
-    static completeTask(req, res) {}
+    static getListTasks(req, res) {
+        const { listId } = req.body;
 
-    static editTask(req, res) {}
+        Task
+            .find({ listId })
+            .select('-_id -__v')
+            .then((result) => {
+                res.status(200).json({
+                    success: true,
+                    data: {
+                        result,
+                    },
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json({
+                    success: false,
+                    message: 'Failed',
+                });
+            });
+    }
 
-    static removeTask(req, res) {}
+    static getTask(req, res) {
+        const { taskId } = req.params;
+
+        Task
+            .find({ taskId })
+            .select('-_id -__v')
+            .then((result) => {
+                res.status(200).json({
+                    success: true,
+                    data: {
+                        result,
+                    },
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json({
+                    success: false,
+                    message: 'Failed',
+                });
+            });
+    }
+
+    static completeTask(req, res) {
+        const { taskId } = req.params;
+
+        Task
+            .findOneAndUpdate({ taskId }, { completed: true }, { new: true })
+            .then((result) => {
+                res.status(200).json({
+                    success: true,
+                    data: {
+                        result,
+                    },
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json({
+                    success: false,
+                    message: 'Failed',
+                });
+            });
+    }
+
+    static editTask(req, res) {
+        const { taskId, title, details, dueDate } = req.params;
+
+        const update = {
+            title,
+            details: details ? details : '',
+            dueDate: dueDate ? dueDate : 0,
+        };
+
+        Task
+            .findOneAndUpdate({ taskId }, update, { new: true })
+            .then((result) => {
+                res.status(200).json({
+                    success: true,
+                    data: {
+                        result,
+                    },
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json({
+                    success: false,
+                    message: 'Failed',
+                });
+            });
+    }
+
+    static removeTask(req, res) {
+        const { taskId } = req.params;
+
+        Task
+            .remove({ taskId })
+            .then(() => {
+                res.status(200).json({
+                    success: true,
+                    message: 'Task deleted',
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json({
+                    success: false,
+                    message: 'Failed',
+                });
+            });
+    }
 
 }
